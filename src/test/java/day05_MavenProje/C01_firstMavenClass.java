d1521225c933e9319a1b13f9a380528402ec47f3
talfik2 <emregondermez@gmail.com>
2022-01-21 20:53:12 +0100

    21.01.2022

--- src/test/java/day05_MavenProje/C01_firstMavenClass.java
+++ src/test/java/day05_MavenProje/C01_firstMavenClass.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
 import java.util.List;
 
 
@@ -15,7 +16,7 @@ public class C01_firstMavenClass {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-      //  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+      driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
         //1- https://www.amazon.com/ sayfasina gidelim
         driver.navigate().to("https://www.amazon.com/");
